9edbbcd297f943e41068b0b1e6739d3c2937cd89
Saar Shen <saars@microsoft.com>
2024-10-16 11:18:13 -0700

    Update the service profiler options a bit

--- Azure.Monitor.OpenTelemetry.Profiler.AspNetCore/ServiceProfilerOptions.cs
+++ Azure.Monitor.OpenTelemetry.Profiler.AspNetCore/ServiceProfilerOptions.cs
@@ -1,6 +1,7 @@
+using Microsoft.ApplicationInsights.Profiler.Shared.Contracts;
+
 namespace Azure.Monitor.OpenTelemetry.Profiler.AspNetCore;
 
-public class ServiceProfilerOptions
+public class ServiceProfilerOptions : UserConfigurationBase
 {
-    public TimeSpan Duration { get; set; } = TimeSpan.FromSeconds(30);
 }
