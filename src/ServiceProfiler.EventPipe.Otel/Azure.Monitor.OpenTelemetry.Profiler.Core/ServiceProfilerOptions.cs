ea40884ccc9a4a1bebb5efab417c45cff371397e
Saar Shen <saars@microsoft.com>
2024-12-02 16:15:36 -0800

    The auth token provider accepts aad audience configuration

--- src/ServiceProfiler.EventPipe.Otel/Azure.Monitor.OpenTelemetry.Profiler.Core/ServiceProfilerOptions.cs
+++ src/ServiceProfiler.EventPipe.Otel/Azure.Monitor.OpenTelemetry.Profiler.Core/ServiceProfilerOptions.cs
@@ -13,5 +13,13 @@ public class ServiceProfilerOptions : UserConfigurationBase
     /// </summary>
     public string? ConnectionString { get; set; } = string.Empty;
 
+    /// <summary>
+    /// Get or sets the value of <see cref="TokenCredential" />.
+    /// If <see cref="TokenCredential" /> is not set, AAD authentication is disabled
+    /// and Instrumentation Key from the Connection String will be used.
+    /// </summary>
+    /// <remarks>
+    /// <see href="https://learn.microsoft.com/en-us/azure/azure-monitor/app/sdk-connection-string?tabs=net#is-the-connection-string-a-secret"/>.
+    /// </remarks>
     public TokenCredential? Credential { get; set; }
 }
