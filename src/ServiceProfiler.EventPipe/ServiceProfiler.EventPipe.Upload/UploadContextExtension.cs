4718eed65e519d9efbbb402d4c97eaf2c9076a0b
Saar Shen <saars@microsoft.com>
2025-06-13 11:48:49 -0700

    Wipe out EP_OTEL_PROFILER constant

--- src/ServiceProfiler.EventPipe/ServiceProfiler.EventPipe.Upload/UploadContextExtension.cs
+++ src/ServiceProfiler.EventPipe/ServiceProfiler.EventPipe.Upload/UploadContextExtension.cs
@@ -1,13 +1,7 @@
-#nullable enable
-
 using System;
-using System.Reflection.Metadata;
 using Azure.Core;
 using Microsoft.ApplicationInsights.Profiler.Core.Contracts;
-
-#if EP_OTEL_PROFILER
 using Microsoft.ApplicationInsights.Profiler.Shared.Contracts;
-#endif
 
 namespace Microsoft.ApplicationInsights.Profiler.Uploader
 {
@@ -21,36 +15,9 @@ namespace Microsoft.ApplicationInsights.Profiler.Uploader
         public UploadContext UploadContext { get; }
         public Guid VerifiedAppId { get; set; }
 
-#if EP_OTEL_PROFILER
-        [Obsolete($"Use {nameof(TokenCredential)} instead.", error: true)]
-#endif
-        public AccessToken? AccessToken { get; set; }
 
-
-#if EP_OTEL_PROFILER
         public IPCAdditionalData? AdditionalData { get; set; }
 
         public TokenCredential? TokenCredential { get; set; } = null;
-#else
-        private TokenCredential? _tokenCredential;
-        public TokenCredential? TokenCredential
-        {
-            get
-            {
-                if (_tokenCredential != null)
-                {
-                    return _tokenCredential;
-                }
-
-                return AccessToken is null || string.IsNullOrEmpty(AccessToken.Value.Token)
-                    ? null
-                    : new StaticAccessTokenCredential(new AccessToken(AccessToken.Value.Token, AccessToken.Value.ExpiresOn));
-            }
-            set
-            {
-                _tokenCredential = value;
-            }
-        }
-#endif
     }
 }
