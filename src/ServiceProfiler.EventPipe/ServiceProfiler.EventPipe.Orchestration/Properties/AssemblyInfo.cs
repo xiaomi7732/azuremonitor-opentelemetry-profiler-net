89133446a46f1f2e2c4535c14c9a7d870be086d0
Saar Shen <saars@microsoft.com>
2025-06-20 11:26:43 -0700

    Remove deprecated orchestration project

--- src/ServiceProfiler.EventPipe/ServiceProfiler.EventPipe.Orchestration/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8fc21fe9-4dd7-4465-9fd0-7bf65a813cce")]
